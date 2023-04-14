eebc26a6a4536012bed6f39a3f3ddfdb633c5d6f
Michał Siwek <mjzsiwek@gmail.com>
2023-04-14 20:51:11 +0200

    Finished initial problem and solution generation

--- src/knapsack.cpp
+++ src/knapsack.cpp
@@ -10,10 +10,18 @@ Knapsack::Knapsack(int capacity, std::vector<Box> boxes) {
 
 void Knapsack::printKnapsack() {
     // Knapsack capacity
-    std::cout << "Knapsack capacity: " << capacity << "kg" << std::endl;
+    std::cout << std::endl << "Knapsack capacity: " << capacity << "kg" << std::endl;
+    // Variables for sum of weight and price
+    int summedWeight = 0;
+    int summedPrice = 0;
     // Boxes inside knapsack
     for(std::vector<Box>::size_type i = 0; i != boxes.size(); i++) {
-        std::cout << "Box " << i << " weight: " << boxes[i].getWeight() << "kg" << std::endl;
-        std::cout << "Box " << i << " price: " << boxes[i].getPrice() << "$" << std::endl;
+        std::cout << "Box " << i << " weight: " << boxes[i].getWeight() << "kg" <<
+        " | price: " << boxes[i].getPrice() << "$" << std::endl;
+        summedWeight += boxes[i].getWeight();
+        summedPrice += boxes[i].getPrice();
     }
+    // Printing summed weight and price
+    std::cout << std::endl << "Total weight: " << summedWeight << "kg" << std::endl;
+    std::cout << "Total price: " << summedPrice << "$";
 }
--- src/problem.cpp
+++ src/problem.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <iostream>
 #include "problem.h"
 
 Problem::Problem() {
@@ -14,31 +15,56 @@ std::vector<Box> Problem::generatePossibleBoxRange() {
     int amountOfBoxes = randomizer.generateIntegerNumberFromRange(3, 5);
 
     for (int i = 1; i <= amountOfBoxes; i++) {
-        int boxWeight = randomizer.generateIntegerNumberFromRange(5, 10);
-        int boxPrice = randomizer.generateIntegerNumberFromRange(1, 5);
+        int boxWeight = randomizer.generateIntegerNumberFromRange(5, 15);
+        // Preventing from identical size of the box
+        while (std::find_if(boxRange.begin(), boxRange.end(), [boxWeight](Box &box) {
+            return box.getWeight() == boxWeight;
+        }) != boxRange.end()) {
+            boxWeight = randomizer.generateIntegerNumberFromRange(5, 15);
+        }
+        int boxPrice = randomizer.generateIntegerNumberFromRange(1, 10);
 
         boxRange.push_back(Box(boxWeight, boxPrice));
     }
 
     // Sorting box range from the heaviest to the lightest
-    std::sort(boxRange.begin(), boxRange.end(), [](Box& box1, Box& box2) -> bool {
-        return (box1.getWeight() > box2.getWeight());
+    std::sort(boxRange.begin(), boxRange.end(), [](Box &box1, Box &box2) {
+        return (box1.getWeight() < box2.getWeight());
     });
 
     return boxRange;
 }
 
 Knapsack Problem::generateKnapsack() {
-    int knapsackCapacity = randomizer.generateIntegerNumberFromRange(10, 25);
+    int knapsackCapacity = randomizer.generateIntegerNumberFromRange(25, 55);
 
     return Knapsack(knapsackCapacity, generateRandomBoxesCollection(knapsackCapacity));
 }
 
 std::vector<Box> Problem::generateRandomBoxesCollection(int maxWeight) {
     std::vector<Box> boxesCollection = std::vector<Box>();
+    int currentBoxesWeight = 0;
 
     for(std::vector<Box>::size_type i = 0; i != possibleBoxRange.size(); i++) {
-        
+        std::cout << "Box " << i << " weight: " << possibleBoxRange[i].getWeight() << "kg" << std::endl;
+    }
+
+    while (maxWeight >= currentBoxesWeight + possibleBoxRange[0].getWeight()) {
+        int boxIndex = randomizer.generateIntegerNumberFromRange(0, possibleBoxRange.size() - 1);
+
+        if (maxWeight >= currentBoxesWeight + possibleBoxRange[boxIndex].getWeight()) {
+            boxesCollection.push_back(possibleBoxRange[boxIndex]);
+            currentBoxesWeight += possibleBoxRange[boxIndex].getWeight();
+
+            continue;
+        }
+        else if (maxWeight >= currentBoxesWeight + possibleBoxRange[0].getWeight()) {
+            boxesCollection.push_back(possibleBoxRange[0]);
+            currentBoxesWeight += possibleBoxRange[0].getWeight();
+
+            continue;
+        }
+        break;
     }
 
     return boxesCollection;
